1bc802fb50dab62cc869c2c66bed68d460379b1e
Racnagore <racnagore@gmail.com>
2017-02-28 13:34:20 +0300

    Added new slides, tried to build slick slider within another slick slider.

--- 2511cargo/js/js.js
+++ 2511cargo/js/js.js
@@ -9,4 +9,86 @@ $('.open-menu-button').click(function(){
             $('.close-menu-button').removeClass('show');
             $('.overlay').fadeOut(300);
         });
+    $('.slide-slider').slick({//слайдер картинки 
+        slidesToShow:1,
+        slidesToScroll:1, 
+        fade:true, 
+        asNavFor: '.slide-line-slider', 
+        focusOnSelect:true,
+        arrows:false
+      }); 
+    $('.slide-line-slider').slick({//слайдер текст 
+        slidesToShow:5, 
+        slidesToScroll: 1, 
+        focusOnSelect:true, 
+        asNavFor: '.slide-slider', 
+        arrows:false 
+    });
+     $('.slider .container').slick({//слайдер текст 
+        arrows:true,
+        prevArrow:$('.slider .slider-left'), 
+        nextArrow:$('.slider .slider-right'),
+        slidesToShow:1, 
+        slidesToScroll: 1, 
+        focusOnSelect:true,
+        swipe: false
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        loop:true,
+        loopedSlides: 4, //looped slides should be the same  
+        grabCursor: true,
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        touchRatio: 0.2,
+        loop:true,
+        loopedSlides: 4, //looped slides should be the same
+        slideToClickedSlide: true,
+        grabCursor: true,
+        slideToClickedSlide: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+            },
+            400: {
+                slidesPerView: 2,
+            }
+        }
+    });
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
+//    $('.line-slider .container').slick({
+//    infinte: true,
+//    speed: 300,
+//    slidesToShow: 4,
+//    slidesToScroll: 1,
+//    asNavFor: '.line-slider .container',
+//    arrows: true,
+//    responsive: [
+//        {
+//            breakpoint: 979,
+//            settings: {
+//                slidesToShow: 3,
+//                infinte: true,
+//                
+//            }
+//        },
+//        {
+//            breakpoint: 767,
+//            settings: {
+//                slidesToShow: 2,
+//                infinte: true
+//            }
+//        },
+//        {
+//            breakpoint: 535,
+//            settings: {
+//                slidesToShow: 1,
+//                infinte: true
+//            }
+//        }
+//    ]
+//});                     
 });
