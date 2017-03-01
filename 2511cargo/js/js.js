2fa24ee69e050d66749e368c00f88a6bdb4a8502
Racnagore <racnagore@gmail.com>
2017-03-01 13:24:46 +0300

    The problem with sliders was solved with return function.

--- 2511cargo/js/js.js
+++ 2511cargo/js/js.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
 $('.open-menu-button').click(function(){
             $('.menu-with-button').addClass('open');
             $('.close-menu-button').addClass('show');
@@ -9,20 +10,12 @@ $('.open-menu-button').click(function(){
             $('.close-menu-button').removeClass('show');
             $('.overlay').fadeOut(300);
         });
-    $('.slide-slider').slick({//слайдер картинки 
-        slidesToShow:1,
-        slidesToScroll:1, 
-        fade:true, 
-        asNavFor: '.slide-line-slider', 
-        focusOnSelect:true,
-        arrows:false
-      }); 
-    $('.slide-line-slider').slick({//слайдер текст 
-        slidesToShow:5, 
-        slidesToScroll: 1, 
-        focusOnSelect:true, 
-        asNavFor: '.slide-slider', 
-        arrows:false 
+	$('.slider .container').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+        var $elSlide = $(slick.$slides[currentSlide]);
+        var sliderObj = $elSlide.closest('.slick-slider');
+        if (sliderObj.hasClass('slide')) {
+    return;
+        }
     });
      $('.slider .container').slick({//слайдер текст 
         arrows:true,
@@ -31,34 +24,55 @@ $('.open-menu-button').click(function(){
         slidesToShow:1, 
         slidesToScroll: 1, 
         focusOnSelect:true,
-        swipe: false
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-        spaceBetween: 10,
-        loop:true,
-        loopedSlides: 4, //looped slides should be the same  
-        grabCursor: true,
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        slidesPerView: 4,
-        touchRatio: 0.2,
-        loop:true,
-        loopedSlides: 4, //looped slides should be the same
-        slideToClickedSlide: true,
-        grabCursor: true,
-        slideToClickedSlide: true,
-        breakpoints: {
-            768: {
-                slidesPerView: 3,
-            },
-            400: {
-                slidesPerView: 2,
-            }
-        }
-    });
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
+        swipe: false,
+	  });
+		$('.slider .container .slide').each(function(i,val){
+		//в каждом сладе находим сладеры
+		var bigSlider=$(val).find('.gallery-top');
+		var listImg=$(val).find('.gallery-thumbs');
+		bigSlider.slick({
+			slidesToShow:1,
+			slidesToScroll:1, 
+			fade:true, 
+			focusOnSelect:true,
+			arrows:false, 
+			asNavFor:listImg //опция использовать указанный сладер как навигацию для этого
+		});
+		listImg.slick({
+			slidesToShow:5, 
+			slidesToScroll: 1, 
+			focusOnSelect:true, 
+			arrows:false,
+			asNavFor:bigSlider //опция использовать указанный сладер как навигацию для этого
+		});
+	});
+});
+//    var galleryTop = new Swiper('.gallery-top', {
+//        spaceBetween: 10,
+//        loop:true,
+//        loopedSlides: 6, //looped slides should be the same  
+//        grabCursor: true,
+//    });
+//    var galleryThumbs = new Swiper('.gallery-thumbs', {
+//        spaceBetween: 1,
+//        slidesPerView: 5,
+//        touchRatio: 0.2,
+//        loop:true,
+//        loopedSlides: 6, //looped slides should be the same
+//        slideToClickedSlide: true,
+//        grabCursor: true,
+//        slideToClickedSlide: true,
+//        breakpoints: {
+//            768: {
+//                slidesPerView: 4,
+//            },
+//            400: {
+//                slidesPerView: 3,
+//            }
+//        }
+//    });
+//    galleryTop.params.control = galleryThumbs;
+//    galleryThumbs.params.control = galleryTop;
 //    $('.line-slider .container').slick({
 //    infinte: true,
 //    speed: 300,
@@ -91,4 +105,3 @@ $('.open-menu-button').click(function(){
 //        }
 //    ]
 //});                     
-});
